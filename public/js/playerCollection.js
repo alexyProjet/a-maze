b4e9982da73dc0a7c5d7dacd12ce4408be90a705
alexyprojet <alexy_duhamel@outlook.fr>
2020-06-26 21:01:39 +0200

    test

---
+++ public/js/playerCollection.js
@@ -0,0 +1,12 @@
+/** litéralelent un Array mais avec des méthodes et attributs en plus*/
+class PlayerCollection extends Array{
+
+    get mainPlayerID(){
+
+    }
+
+    set mainPlayerID(ID_){
+        
+    }
+    
+}
